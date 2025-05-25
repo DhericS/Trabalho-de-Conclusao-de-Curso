d60f2a3075862911c83c91ee9e198797fb5a2201
dheric <dhericdrake@gmail.com>
2025-05-25 18:07:32 -0300

    FIX: corrigindo erros do front

--- src/main/java/com/example/NovoTesteCrud/controller/AutenticacaoController.java
+++ src/main/java/com/example/NovoTesteCrud/controller/AutenticacaoController.java
@@ -185,7 +185,7 @@ public class AutenticacaoController {
         String token = java.util.UUID.randomUUID().toString();
         resetarSenhaRedisService.salvarToken(token, request.getEmail(), 60);
 
-        String resetLink = "http://guidesfit.com.br/redefinir-senha?token=" + token;
+        String resetLink = "https://guidesfit.com.br/redefinir-senha?token=" + token;
 
         emailService.sendEmail(
                 request.getEmail(),
